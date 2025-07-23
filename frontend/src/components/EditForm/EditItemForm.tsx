a2668e55d04bb08062fea424271edd8c4c68ffe2
Artemy-And <andruschak.artemy@gmail.com>
2025-07-23 11:44:01 +0300

    added eslint + configurations

--- frontend/src/components/EditForm/EditItemForm.tsx
+++ frontend/src/components/EditForm/EditItemForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import s from "./editForm.module.scss"
 
 const EditItemForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm({
@@ -25,60 +26,61 @@ const EditItemForm = () => {
 
       <form onSubmit={handleSubmit(onSubmit)}>
         <DialogContent>
-     
-            <DialogHeader>
-              <DialogTitle>Edit item</DialogTitle>
-              <DialogDescription>
-                {/* This action cannot be undone. This will permanently delete your account
+
+          <DialogHeader>
+            <DialogTitle>Edit item</DialogTitle>
+            <DialogDescription>
+              {/* This action cannot be undone. This will permanently delete your account
                 and remove your data from our servers. */}
-              </DialogDescription>
-            </DialogHeader>
-            <div>
-                <input type="text" id="title" {...register("title", { required: "Title is required" })} />
+            </DialogDescription>
+          </DialogHeader>
+          <div>
+            <input type="text" id="title" {...register("title", { required: "Title is required" })} />
             {errors.title && <p>{errors.title.message}</p>}
-         
 
-          <div>
-            <label htmlFor="url">URL</label>
-            <input type="url" id="url" {...register("url", {
-              required: "URL is required",
-              pattern: {
-                value: /^(http|https):\/\/[^ "]+$/,
-                message: "Invalid URL format"
-              }
-            })} />
-            {errors.url && <p>{errors.url.message}</p>}
-          </div>
 
-          <div>
-            <label htmlFor="description">Description</label>
-            <textarea id="description" {...register("description")} />
-          </div>
-
-          <div>
-            <label htmlFor="comments">Comments</label>
-            <textarea id="comments" {...register("comments")} />
-          </div>
-
-          <div>
-            <label htmlFor="imageUrl">Image URL</label>
-            <input type="url" id="imageUrl" {...register("imageUrl")} /> {/* Changed to type="url" */}
-          </div>
-
-          <div>
-            <label htmlFor="tags">Tags</label>
-            <input type="text" id="tags" {...register("tags")} /> {/* Changed to type="text" */}
-          </div>
-
+            <div>
+              <label htmlFor="url">URL</label>
+              <input type="url" id="url" {...register("url", {
+                required: "URL is required",
+                pattern: {
+                  value: /^(http|https):\/\/[^ "]+$/,
+                  message: "Invalid URL format"
+                }
+              })} />
+              {errors.url && <p>{errors.url.message}</p>}
             </div>
-          
-          {/* ... (created at and updated at fields) */}
 
-          <button type="submit">Save & Back</button>
-          <button type="button">Save as Copy</button>
-          <button type="submit">Save</button>
-          <button type="button">Back</button>
-          <button type="button">Delete</button>
+            <div>
+              <label htmlFor="description">Description</label>
+              <textarea id="description" {...register("description")} />
+            </div>
+
+            <div>
+              <label htmlFor="comments">Comments</label>
+              <textarea id="comments" {...register("comments")} />
+            </div>
+
+            <div>
+              <label htmlFor="imageUrl">Image URL</label>
+              <input type="url" id="imageUrl" {...register("imageUrl")} /> {/* Changed to type="url" */}
+            </div>
+
+            <div>
+              <label htmlFor="tags">Tags</label>
+              <input type="text" id="tags" {...register("tags")} /> {/* Changed to type="text" */}
+            </div>
+
+          </div>
+
+          {/* ... (created at and updated at fields) */}
+          <div className={s.btnsFooter}>      
+            <button type="submit">Save & Back</button>
+            <button type="button">Save as Copy</button>
+            <button type="submit">Save</button>
+            <button type="button">Back</button>
+            <button type="button">Delete</button></div>
+
         </DialogContent>
       </form>
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      // "@redux": path.resolve(__dirname, "./src/redux"),
     },
   },
 })
