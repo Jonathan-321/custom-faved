f12d35419913e22c420840dd0bf81e0cb670bab9
Denis Dvali <2803994+denho@users.noreply.github.com>
2025-09-10 20:03:10 +0400

    Fix card appearance

--- frontend/src/components/Table/CardView.tsx
+++ frontend/src/components/Table/CardView.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
+import { CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -21,61 +21,62 @@ export const CardView: React.FC<{ setIsShowEditModal: (val: boolean) => void; el
     const store = React.useContext(StoreContext);
     return (
 
-        <div>
-            <CardAction>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button
-                            variant="link"
-                            className="data-[state=open]:bg-muted text-muted-foreground flex size-8 absolute top-2 right-2"
-                            size="icon"
-                        >
-                            <IconDotsVertical />
-                            <span className="sr-only">Open menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end" className="w-32">
-                        <DropdownMenuItem
-                            onClick={() => {
-                                store.setType(ActionType.EDIT);
-                                setIsShowEditModal(true);
-                                store.setIdItem(el.id);
-                            }}
-                        >
-                            Edit
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => { store.onCreateItem(el, true, false, null); }}>Make a copy</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <AlertDialog>
-                            <AlertDialogTrigger asChild>
-                                <div
-                                    className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-destructive/90 hover:text-white"
-                                >
-                                    Delete
-                                </div>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogHeader>
-                                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        This action cannot be undone. This will permanently delete your item.
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                    <AlertDialogAction
-                                        onClick={() => store.onDeleteItem(el.id)}
-                                        className="w-full sm:w-auto order-first sm:order-last mt-2 sm:mt-0 bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60"
+        <div className='flex flex-col justify-between h-full'>
+
+            <CardHeader>
+                <CardAction>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                              variant="link"
+                              className="data-[state=open]:bg-muted text-muted-foreground flex size-8 absolute top-2 right-2"
+                              size="icon"
+                            >
+                                <IconDotsVertical />
+                                <span className="sr-only">Open menu</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="w-32">
+                            <DropdownMenuItem
+                              onClick={() => {
+                                  store.setType(ActionType.EDIT);
+                                  setIsShowEditModal(true);
+                                  store.setIdItem(el.id);
+                              }}
+                            >
+                                Edit
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => { store.onCreateItem(el, true, false, null); }}>Make a copy</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                    <div
+                                      className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-destructive/90 hover:text-white"
                                     >
                                         Delete
-                                    </AlertDialogAction>
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialog>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </CardAction>
-            <CardHeader>
+                                    </div>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            This action cannot be undone. This will permanently delete your item.
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                        <AlertDialogAction
+                                          onClick={() => store.onDeleteItem(el.id)}
+                                          className="w-full sm:w-auto order-first sm:order-last mt-2 sm:mt-0 bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60"
+                                        >
+                                            Delete
+                                        </AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </CardAction>
                 <div className="flex flex-column flex-wrap sm:flex-row md:flex-row kg:flex-row items-start w-full">
                     {el.image && (
                         <div className="flex w-[175px] pb-4 md:pr-4 lg:pr-4  w-full">
@@ -85,7 +86,7 @@ export const CardView: React.FC<{ setIsShowEditModal: (val: boolean) => void; el
                     <div className="flex flex-col items-start flex-wrap table-layout-fixed break-words">
                         <CardTitle>
                             {el.title && (
-                                <span className="flex items-start text-left w-full flex-wrap pb-2 break-words break-all">
+                                <span className="flex items-start text-left w-full flex-wrap break-words break-all">
                                     <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
                                         {el.title}
                                     </h4>
@@ -93,7 +94,7 @@ export const CardView: React.FC<{ setIsShowEditModal: (val: boolean) => void; el
                             )}
                         </CardTitle>
                         {el.url && (
-                            <span className="flex items-start text-left w-full flex-wrap pb-2 break-all">
+                            <span className="flex items-start text-left w-full flex-wrap py-2 break-all">
                                 <a
                                     className="text-custom-blue underline"
                                     href={el.url}
@@ -105,13 +106,13 @@ export const CardView: React.FC<{ setIsShowEditModal: (val: boolean) => void; el
                             </span>
                         )}
                         {el.tags && (
-                            <div className="flex flex-start text-left w-full flex-wrap pb-2">
+                            <div className="flex flex-start text-left w-full flex-wrap py-2">
                                 {el.tags.map((tagID, index) => (
                                     <TagBadge key={index} tagID={tagID} />
                                 ))}
                             </div>
                         )}
-                        <CardDescription>
+                        { (el.description || el.comments) && (<CardDescription>
                             <div className="flex flex-col items-start w-full flex-wrap break-words break-all">
                                 <div className="flex flex-col items-start text-start">
                                     <div>
@@ -124,7 +125,7 @@ export const CardView: React.FC<{ setIsShowEditModal: (val: boolean) => void; el
                                     </div>
                                 </div>
                             </div>
-                        </CardDescription>
+                        </CardDescription>)}
                     </div>
                 </div>
             </CardHeader>
