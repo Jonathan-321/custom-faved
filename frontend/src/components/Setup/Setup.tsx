09ee8a6f89594809ac2797804469ce04b7fa4498
Denis Dvali <2803994+denho@users.noreply.github.com>
2025-09-17 18:13:09 +0400

    Improve setup page display

--- frontend/src/components/Setup/Setup.tsx
+++ frontend/src/components/Setup/Setup.tsx
@@ -32,7 +32,7 @@ export const Setup = observer(({ className, ...props }: React.ComponentProps<"di
 
     return (
 
-      <div className="flex flex-col items-center justify-center text-left min-h-svh w-full p-3 gap-8">
+      <div className="flex flex-col items-center justify-start text-left min-h-svh w-full px-3 py-12 gap-8">
           <div className="flex items-center justify-center gap-2.5">
               <img
                 src="logo.png"
@@ -43,20 +43,18 @@ export const Setup = observer(({ className, ...props }: React.ComponentProps<"di
           </div>
           <Card className='max-w-3xl'>
               <CardHeader>
-                  <CardTitle>Welcome to Faved!</CardTitle>
+                  <CardTitle className="text-lg">Welcome to Faved!</CardTitle>
               </CardHeader>
-              <CardContent className="flex flex-col gap-4">
-                  <p className="mb-4">Before you can use the application, we need to set up the database. Click the button below to proceed.</p>
+              <CardContent className="flex flex-col gap-8">
+                  <p>Before you can use the application, we need to set up the database. Click the button below to proceed.</p>
 
-                  <div className="bg-blue-100 p-4 rounded-lg mb-4 dark:bg-blue-600">
-                      <p className="italic mb-2"><strong>Note:</strong> This will create a database file in the <code>/storage</code> folder of your application installation directory.</p>
+                  <div className="bg-blue-100 p-4 rounded-lg  dark:bg-blue-600">
+                      <p className="italic"><strong>Note:</strong> This will create a database file in the <code>/storage</code> folder of your application installation directory.</p>
                   </div>
-              </CardContent>
-              <CardFooter>
                   <Button className="bg-black hover:bg-black text-white font-bold py-2 px-4 rounded w-full" disabled={isLoading} onClick={ submit }>
                       {isLoading ? 'Creating Database...' : 'Create Database'}
                   </Button>
-              </CardFooter>
+              </CardContent>
           </Card>
       </div>
     )
